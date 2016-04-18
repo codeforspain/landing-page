5e313b0699c73efb713bf0fd64fdefcdad7cfc56
Enrique Bernabeu <bitdev.contacto@gmail.com>
2016-04-18 13:39:49 +0200

    Add feedback actions & classes

--- scripts/main.js
+++ scripts/main.js
@@ -8,11 +8,28 @@ $(() => {
 
         e.preventDefault();
 
+        var btn = document.getElementById('btn-email'); // Select #btn-email object
+        var input = document.getElementById('email'); // Select #email object
+        var response = document.getElementById('response-text'); // Select #response-text object
+
+        input.className = ''; // Remove all classes to #email
+        response.className = ''; // Remove all classes to #response-text
+        response.className = ''; // Remove all classes to to #response-text
+        response.innerHTML = ''; // Remove #response-text text
+
+        btn.innerHTML = 'ENVIANDO...'; // Feedback response in #btn-email
+        btn.className = 'send'; // Add feedback response class to #btn-email
+
         let $form = $(e.target);
         let email = $form.find('input[name="email"]').val();
 
         if ( !email.match(re) ) {
-             alert('El email no es válido');
+             // alert('El email no es válido');
+             btn.innerHTML = 'CONSEGUIR INVITACIÓN'; // Return to the old #btn-email text
+             btn.className = btn.className.replace("send", ""); // Remove feedback response class to #btn-email
+             input.className = 'error'; // Add .error class to #email
+             response.innerHTML = 'El email no es válido'; // Add error text to #response-text
+             response.className = 'error'; // Add error class to #response-text
              return false;
         }
 
@@ -20,7 +37,11 @@ $(() => {
         let data = $form.serialize();
         $.post(url, data);
 
-        alert('Invitación enviada');
+        // alert('Invitación enviada');
+        input.className = ''; // Remove all classes to #email
+        response.innerHTML = 'Invitacion enviada.'; // Add success text to #response-text
+        response.className = 'success'; // Add .success class to #response-text
+
     };
 
     let attachFormEvents = ($form) => {
